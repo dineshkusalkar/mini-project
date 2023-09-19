428406bfae8eb116f3f3857edbc048654fcb6cf4
Dinesh k <dineshmk18@outlook.com>
2023-09-19 19:02:54 +0000

    Updated secrets.tf

--- master/terraform_files/secrets.tf
+++ master/terraform_files/secrets.tf
@@ -2,8 +2,7 @@ resource "azurerm_key_vault_secret" "secret1" {
   name         = "username"
   value        = "dinesh"
   key_vault_id = azurerm_key_vault.AKV.id
-  depends_on   = [azurerm_key_vault.AKV, azurerm_role_assignment.ara, azurerm_key_vault_access_policy.example-app-principal, azurerm_role_assignment.ara2, azurerm_key_vault_access_policy.AKS-Agentpool-principal]
-
+  depends_on   = [azurerm_key_vault.AKV]
 
 }
 
@@ -11,8 +10,7 @@ resource "azurerm_key_vault_secret" "secret2" {
   name         = "user-password"
   value        = "Banglore#1998"
   key_vault_id = azurerm_key_vault.AKV.id
-  depends_on   = [azurerm_key_vault.AKV, azurerm_role_assignment.ara, azurerm_key_vault_access_policy.example-app-principal, azurerm_role_assignment.ara2, azurerm_key_vault_access_policy.AKS-Agentpool-principal]
-
+  depends_on   = [azurerm_key_vault.AKV]
 
 }
 
@@ -20,7 +18,6 @@ resource "azurerm_key_vault_secret" "secret3" {
   name         = "root-password"
   value        = "Maharashtra1998@"
   key_vault_id = azurerm_key_vault.AKV.id
-  depends_on   = [azurerm_key_vault.AKV, azurerm_role_assignment.ara, azurerm_key_vault_access_policy.example-app-principal, azurerm_role_assignment.ara2, azurerm_key_vault_access_policy.AKS-Agentpool-principal]
-
+  depends_on   = [azurerm_key_vault.AKV]
 
 }
