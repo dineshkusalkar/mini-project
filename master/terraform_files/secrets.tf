2fead3ca290518d0f417403118a9a0794dee1318
Dinesh k <dineshmk18@outlook.com>
2023-09-19 19:06:08 +0000

    Updated secrets.tf

--- master/terraform_files/secrets.tf
+++ master/terraform_files/secrets.tf
@@ -1,4 +1,4 @@
-resource "azurerm_key_vault_secret" "secret1" {
+resource "azurerm_key_vault_secret" "secrets1" {
   name         = "username"
   value        = "dinesh"
   key_vault_id = azurerm_key_vault.AKV.id
@@ -6,7 +6,7 @@ resource "azurerm_key_vault_secret" "secret1" {
 
 }
 
-resource "azurerm_key_vault_secret" "secret2" {
+resource "azurerm_key_vault_secret" "secrets2" {
   name         = "user-password"
   value        = "Banglore#1998"
   key_vault_id = azurerm_key_vault.AKV.id
@@ -14,7 +14,7 @@ resource "azurerm_key_vault_secret" "secret2" {
 
 }
 
-resource "azurerm_key_vault_secret" "secret3" {
+resource "azurerm_key_vault_secret" "secrets3" {
   name         = "root-password"
   value        = "Maharashtra1998@"
   key_vault_id = azurerm_key_vault.AKV.id
