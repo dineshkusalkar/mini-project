e8abfc83ad8bd9ff7d568e7d92d23b6db7f0507c
Dinesh k <dineshmk18@outlook.com>
2023-09-22 11:15:26 +0000

    Updated main.tf

---
+++ QA/terraform/main.tf
@@ -0,0 +1,36 @@
+# Keep the terraform state file at a centralised location
+# here i created storage account manually and provide details in backend block
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "backend-statefile"
+    storage_account_name = "terraformstate1998"
+    container_name       = "backend"
+    key                  = "terraform.tfstate"
+
+  }
+}
+
+
+provider "azurerm" {
+  features {}
+}
+
+module "RG"{
+    source = "./modules/RG"
+    
+}
+
+module "AKS"{
+    source = "./modules/AKS"
+
+}
+
+module "AKV"{
+    source = "./modules/AKV"
+    
+}
+
+module "secrets"{
+    source = "./modules/secrets"
+    
+}
