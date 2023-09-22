f812bdd6b819bf86a32eebeca91ad20669a87dfc
Dinesh k <dineshmk18@outlook.com>
2023-09-22 10:54:55 +0000

    Updated AKV.tf

--- modules/AKV/AKV.tf
+++ modules/AKV/AKV.tf
@@ -4,10 +4,10 @@
 data "azurerm_client_config" "current" {}
 
 
-resource "azurerm_key_vault" "AKV-qa" {
-  name                       = "kvdinesh007-QA"
+resource "azurerm_key_vault" "AKV" {
+  name                       = "kvdinesh007"
   location                   = var.location
-  resource_group_name        = azurerm_resource_group.mini-project-qa.name
+  resource_group_name        = azurerm_resource_group.mini-project.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard" 
   soft_delete_retention_days = 7
@@ -15,7 +15,7 @@ resource "azurerm_key_vault" "AKV-qa" {
 
 
   
-  depends_on = [azurerm_kubernetes_cluster.aks-qa]
+  depends_on = [azurerm_kubernetes_cluster.aks]
 
   access_policy {
      tenant_id = data.azurerm_client_config.current.tenant_id
@@ -38,30 +38,26 @@ resource "azurerm_key_vault" "AKV-qa" {
 }
 
 
-/* data "azuread_service_principal" "example" {
-  display_name = "example-app"
-} */
-
 data "azuread_service_principal" "example-app" {
   display_name = "example-app"
 
   # az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/dc272485-d2da-4a98-8171-00ce402c7324" --name example-app
 }
 
-resource "azurerm_role_assignment" "ara-qa" {
-  scope                            = azurerm_key_vault.AKV-qa.id
+resource "azurerm_role_assignment" "ara" {
+  scope                            = azurerm_key_vault.AKV.id
   role_definition_name             = "Contributor"
   principal_id                     = data.azuread_service_principal.example-app.object_id
   skip_service_principal_aad_check = true
-  depends_on = [azurerm_key_vault.AKV-qa]
+  depends_on = [azurerm_key_vault.AKV]
 }
 
 
-resource "azurerm_key_vault_access_policy" "example-app-principal-qa" {
-  key_vault_id = azurerm_key_vault.AKV-qa.id
+resource "azurerm_key_vault_access_policy" "example-app-principal" {
+  key_vault_id = azurerm_key_vault.AKV.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azuread_service_principal.example-app.object_id
-  depends_on = [azurerm_key_vault.AKV-qa]
+  depends_on = [azurerm_key_vault.AKV]
   key_permissions = [
     "Get", "List", "Encrypt", "Decrypt", "Delete"
   ]
@@ -79,19 +75,19 @@ resource "azurerm_key_vault_access_policy" "example-app-principal-qa" {
 }
 
 
-resource "azurerm_role_assignment" "ara2-qa" {
-  scope                = azurerm_key_vault.AKV-qa.id
+resource "azurerm_role_assignment" "ara2" {
+  scope                = azurerm_key_vault.AKV.id
   role_definition_name = "Contributor"
-  principal_id         = azurerm_kubernetes_cluster.aks-qa.kubelet_identity[0].object_id
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   skip_service_principal_aad_check = true
-  depends_on = [azurerm_key_vault.AKV-qa]
+  depends_on = [azurerm_key_vault.AKV]
 }
 
-resource "azurerm_key_vault_access_policy" "AKS-Agentpool-principal-qa" {
-  key_vault_id = azurerm_key_vault.AKV-qa.id
+resource "azurerm_key_vault_access_policy" "AKS-Agentpool-principal" {
+  key_vault_id = azurerm_key_vault.AKV.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_kubernetes_cluster.aks-qa.kubelet_identity[0].object_id #"c253a12a-6063-4235-ac8f-bbee8250f034"   #"67ff6ee4-0312-4d04-9356-78cec1708c1d" # AKS-Agent POOl object ID userAssignedIdentityID
-  depends_on = [azurerm_key_vault.AKV-qa]
+  object_id    = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id 
+  depends_on = [azurerm_key_vault.AKV]
   key_permissions = [
     "Get", "List", "Encrypt", "Decrypt", "Delete"
   ] 
@@ -108,19 +104,3 @@ resource "azurerm_key_vault_access_policy" "AKS-Agentpool-principal-qa" {
   
 } 
 
-/* resource "azurerm_key_vault_access_policy" "example" {
-  key_vault_id = azurerm_key_vault.AKV.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  
-  object_id    = data.azurerm_client_config.current.object_id
-
- secret_permissions = [
-      "Set",
-      "Get",
-      "Delete",
-      "Purge",
-      "Recover",
-      "List"
-    ]
-    
-} */
