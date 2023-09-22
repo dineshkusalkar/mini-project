2acc2a652f3900f4d3e0cb73708025f7c03c55c2
Dinesh k <dineshmk18@outlook.com>
2023-09-22 10:49:14 +0000

    Updated var.tf

--- modules/AKS/var.tf
+++ modules/AKS/var.tf
@@ -9,3 +9,19 @@ variable "cluster_name" {
   description = "AKS name in Azure"
   default     = "AKS"
 }
+
+variable "cluster_name" {
+  type        = string
+  description = "AKS name in Azure"
+  default     = "AKS"
+}
+variable "kubernetes_version" {
+  type        = string
+  description = "Kubernetes version"
+  default     = "1.26.6"
+}
+variable "system_node_count" {
+  type        = number
+  description = "Number of AKS worker nodes"
+  default     = 1
+}
