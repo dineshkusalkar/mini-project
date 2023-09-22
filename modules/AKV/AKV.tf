fb409719a2e4fc06b53a6c2a6c64bd0867c23bd7
Dinesh k <dineshmk18@outlook.com>
2023-09-22 11:00:09 +0000

    Updated AKV.tf

--- modules/AKV/AKV.tf
+++ modules/AKV/AKV.tf
@@ -7,7 +7,7 @@ data "azurerm_client_config" "current" {}
 resource "azurerm_key_vault" "AKV" {
   name                       = var.name
   location                   = var.location
-  resource_group_name        = azurerm_resource_group.mini-project.name
+  resource_group_name        = var.resource_group_name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard" 
   soft_delete_retention_days = 7
