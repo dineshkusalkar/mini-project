6d9153ad0050af939ecfd9747a982fc7d40a6d74
Dinesh k <dineshmk18@outlook.com>
2023-09-24 06:02:26 +0000

    Updated main.tf

--- QA/terraform/main.tf
+++ QA/terraform/main.tf
@@ -22,7 +22,7 @@ provider "azurerm" {
 
 
 module "AKS"{
-    source = "../modules/AKS/"
+    source =  "mini-project/modules/AKS"       #"../modules/AKS/"
     resource_group_name = "mini-project-QA"
     cluster_name = "AKS-QA"
   
@@ -31,7 +31,7 @@ module "AKS"{
 
 
 module "AKV"{
-    source = "../modules/AKV/"
+    source = "mini-project/modules/AKV"       #"../modules/AKV/"
     name = "kvdinesh007-QA"
     cluster_name = "AKS-QA"
     resource_group_name = module.AKS.resource_group_name
