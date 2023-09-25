2b7d5069cf4337d656f11a55c8a0e95cc2a8e6ac
Dinesh k <dineshmk18@outlook.com>
2023-09-25 06:18:48 +0000

    Updated var.tf

--- modules/AKS/var.tf
+++ modules/AKS/var.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   type        = string
   description = "RG name in Azure"
-  default     = "mini_project_PROD"
+  default     = "mini_project"
 }
 
 variable "cluster_name" {
