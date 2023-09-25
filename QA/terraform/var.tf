d62bc5d7f86abb9727fbe8709186d642909a47c9
Dinesh k <dineshmk18@outlook.com>
2023-09-25 09:49:01 +0000

    Added var.tf

---
+++ QA/terraform/var.tf
@@ -0,0 +1,17 @@
+variable "resource_group_name" {  
+  type        = string
+  description = "RG name in Azure"
+  default     = "mini-project-QA"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "AKS name in Azure"
+  default     = "AKS-QA"
+}
+
+variable "name" {
+  type        = string
+  description = "Resources location in Azure"
+  default     = "kvdinesh007-QA"
+}
