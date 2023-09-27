2f863d3dd128a7f49b14a6cf065a4319ebe03e2e
Dinesh k <dineshmk18@outlook.com>
2023-09-27 10:54:17 +0000

    Updated AKV.tf

--- modules/AKV/AKV.tf
+++ modules/AKV/AKV.tf
@@ -1,7 +1,7 @@
-data "azurerm_kubernetes_cluster" "aks" {
-  name = var.cluster_name
-  resource_group_name = var.resource_group_name
-}
+// data "azurerm_kubernetes_cluster" "aks" {
+//   name = var.cluster_name
+//   resource_group_name = var.resource_group_name
+// }
 
 
 data "azurerm_client_config" "current" {}
@@ -18,7 +18,7 @@ resource "azurerm_key_vault" "AKV" {
 
 
   
-   depends_on = [data.azurerm_kubernetes_cluster.aks]
+   // depends_on = [data.azurerm_kubernetes_cluster.aks]
 
   access_policy {
      tenant_id = data.azurerm_client_config.current.tenant_id
