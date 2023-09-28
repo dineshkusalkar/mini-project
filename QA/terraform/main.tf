e10faf9816044212cdfc77f61052c295bd851f3e
Dinesh k <dineshmk18@outlook.com>
2023-09-28 08:14:32 +0000

    Updated main.tf

--- QA/terraform/main.tf
+++ QA/terraform/main.tf
@@ -10,6 +10,15 @@ terraform {
   }
 }
 
+terraform {
+  required_providers {
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+  }
+}
+
 
 provider "azurerm" {
   features {}
