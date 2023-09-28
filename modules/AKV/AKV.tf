38ef4816203646c06ae5bfc310488dacf02abdfc
Dinesh k <dineshmk18@outlook.com>
2023-09-28 13:20:33 +0000

    Updated AKV.tf

--- modules/AKV/AKV.tf
+++ modules/AKV/AKV.tf
@@ -18,6 +18,7 @@ resource "azurerm_key_vault" "AKV" {
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard" 
   soft_delete_retention_days = 7
+  purge_protection_enabled = false
 
 
 
