e127dcab6c1332bc6eb0a875cd5cb8cc91bf4b92
Dinesh k <dineshmk18@outlook.com>
2023-10-01 07:17:45 +0000

    Updated var.tf

--- QA/terraform/var.tf
+++ QA/terraform/var.tf
@@ -1,26 +1,26 @@
-// variable "resource_group_name" {  
-//   type        = string
-//   description = "RG name in Azure"
-//   default     = "mini-project-QA"
-// }
+variable "resource_group_name" {  
+  type        = string
+  description = "RG name in Azure"
+  default     = "mini-project-QA"
+}
 
-// variable "location" {
-//   type        = string
-//   description = "Resources location in Azure"
-//   default     = "eastus"
-// }
+variable "location" {
+  type        = string
+  description = "Resources location in Azure"
+  default     = "eastus"
+}
 
-// variable "cluster_name" {
-//   type        = string
-//   description = "AKS name in Azure"
-//   default     = "AKS-QA"
-// }
+variable "cluster_name" {
+  type        = string
+  description = "AKS name in Azure"
+  default     = "AKS-QA"
+}
 
-// variable "name" {
-//   type        = string
-//   description = "Resources location in Azure"
-//   default     = "kvdinesh007-QA"
-// }
+variable "name" {
+  type        = string
+  description = "Resources location in Azure"
+  default     = "kvdinesh007-QA"
+}
 
 // variable "user_name" {
 //   type        = string
