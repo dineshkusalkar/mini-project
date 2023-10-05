27316ef234a56d672787e99fc7cc5eb8989aae8f
Dinesh k <dineshmk18@outlook.com>
2023-10-05 13:53:43 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ def get_secret(secret_name):
     key_vault_url = os.environ.get("keyvault_url")
 
     credential = ClientSecretCredential(                                                                           # below details are reffer from {example-app} service principle
-        tenant_id= os.environ.get("client_id")                                         #'62c65783-e48b-4438-8d2a-50fb84685b6e',
-        client_id='fbc85d1a-63e6-43b1-b528-35f30e561182',                          
-        client_secret='56-8Q~fiieMS4OtiiCHRBAzXfQgrlaeq3wVTobA_'     
+        tenant_id= '62c65783-e48b-4438-8d2a-50fb84685b6e',
+        client_id= os.environ.get("client_id"),                                         #'fbc85d1a-63e6-43b1-b528-35f30e561182',                          
+        client_secret= 'BJn8Q~JWDcGiUWSR6xkGII~_0WD6bkmNHBuxQbe_'                       #'56-8Q~fiieMS4OtiiCHRBAzXfQgrlaeq3wVTobA_'     
     )
     secret_client = SecretClient(vault_url=key_vault_url, credential=credential)
 
