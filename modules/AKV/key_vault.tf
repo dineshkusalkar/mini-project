144670e4329ae7845ae9ed8ec50d8a8c51e93e76
Dinesh k <dineshmk18@outlook.com>
2023-10-09 13:06:44 +0000

    Updated key_vault.tf

--- modules/AKV/key_vault.tf
+++ modules/AKV/key_vault.tf
@@ -9,7 +9,7 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 
-resource "azurerm_key_vault" "Azure_Key_Vault" {
+resource "azurerm_key_vault" "azure_key_vault" {
   name                       = var.name
   location                   = var.location
   resource_group_name        = var.resource_group_name
@@ -45,19 +45,19 @@ resource "azurerm_key_vault" "Azure_Key_Vault" {
 
 
 resource "azurerm_role_assignment" "role_assignment" {
-  scope                = azurerm_key_vault.Azure_Key_Vault.id        
+  scope                = azurerm_key_vault.azure_key_vault.id        
   role_definition_name = "Contributor"
-  principal_id         = var.principal_id  # azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  principal_id         = var.principal_id  
     
   skip_service_principal_aad_check = true
-  depends_on = [azurerm_key_vault.Azure_Key_Vault]
+  depends_on = [azurerm_key_vault.azure_key_vault]
 }
 
-resource "azurerm_key_vault_access_policy" "AKS-Agentpool-principal" {
-  key_vault_id = azurerm_key_vault.Azure_Key_Vault.id
+resource "azurerm_key_vault_access_policy" "aks-agentpool" {
+  key_vault_id = azurerm_key_vault.azure_key_vault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = var.object_id   # azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  depends_on = [azurerm_key_vault.Azure_Key_Vault]
+  object_id    = var.object_id   
+  depends_on = [azurerm_key_vault.azure_key_vault]
   
 
   secret_permissions = [
@@ -75,21 +75,21 @@ resource "azurerm_key_vault_access_policy" "AKS-Agentpool-principal" {
 resource "azurerm_key_vault_secret" "secret1" {
   name         = "username"
   value        = var.user_name
-  key_vault_id = azurerm_key_vault.Azure_Key_Vault.id
-  depends_on   = [azurerm_key_vault.Azure_Key_Vault]
+  key_vault_id = azurerm_key_vault.azure_key_vault.id
+  depends_on   = [azurerm_key_vault.azure_key_vault]
 }
 
 resource "azurerm_key_vault_secret" "secret2" {
   name         = "user-password"
   value        = var.user_password
-  key_vault_id = azurerm_key_vault.Azure_Key_Vault.id
-  depends_on   = [azurerm_key_vault.Azure_Key_Vault]
+  key_vault_id = azurerm_key_vault.azure_key_vault.id
+  depends_on   = [azurerm_key_vault.azure_key_vault]
 }
 
 resource "azurerm_key_vault_secret" "secret3" {
   name         = "root-password"
   value        = var.user_rootpassword
-  key_vault_id = azurerm_key_vault.Azure_Key_Vault.id
-  depends_on   = [azurerm_key_vault.Azure_Key_Vault]
+  key_vault_id = azurerm_key_vault.azure_key_vault.id
+  depends_on   = [azurerm_key_vault.azure_key_vault]
 }
 
