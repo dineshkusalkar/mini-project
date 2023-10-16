80a3e8d419fe55c0a8c12f611becfb0d422fe597
Dinesh k <dineshmk18@outlook.com>
2023-10-16 11:14:07 +0000

    Updated resource_group.tf

--- modules/RG/resource_group.tf
+++ modules/RG/resource_group.tf
@@ -1,11 +1,8 @@
 resource "azurerm_resource_group" "mini-project" {
   name     = var.resource_group_name
   location = var.location
-
 }
 
-
 output "resource_group_name" {
-  
-  value       = azurerm_resource_group.mini-project.name
+    value       = azurerm_resource_group.mini-project.name
 }
